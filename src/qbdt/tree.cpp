7260bc02520ceb5bed1673159d43393d5dc25798
Daniel Strano <stranoj@gmail.com>
2022-03-12 14:47:59 -0500

    Only branch for stabilizer

--- src/qbdt/tree.cpp
+++ src/qbdt/tree.cpp
@@ -685,9 +685,8 @@ void QBdt::ApplySingle(const complex* mtrx, bitLenInt target)
             return (bitCapInt)0U;
         }
 
-        leaf->Branch();
-
         if (isKet) {
+            leaf->Branch();
             QInterfacePtr qi = NODE_TO_QINTERFACE(leaf);
             if (qis.find(qi) == qis.end()) {
                 try {
@@ -796,9 +795,8 @@ void QBdt::ApplyControlledSingle(
             return (bitCapInt)0U;
         }
 
-        leaf->Branch();
-
         if (isKet) {
+            leaf->Branch();
             QInterfacePtr qi = NODE_TO_QINTERFACE(leaf);
             if (qis.find(qi) == qis.end()) {
                 try {
